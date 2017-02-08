f58df179727db818860422e90c6ddbb2ea0fbed2
nimus0108 <nimus0108@gmail.com>
2017-02-08 09:29:25 -0500

    2017

--- js/countdown.js
+++ js/countdown.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = '2016-09-17T15:00:00.000Z';
+var deadline = '2017-05-16T15:00:00.000Z';
 initializeClock('countdown', deadline);
